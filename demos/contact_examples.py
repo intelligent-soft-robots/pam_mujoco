1cb8d2e927fafa9c57efeac017aae9383ed7835c
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-09-01 11:27:56 +0200

    update

--- demos/contact_examples.py
+++ demos/contact_examples.py
@@ -8,6 +8,7 @@ import multiprocessing
 segment_id_contacts = "contacts"
 segment_id_contacts_reset = "contacts_reset"
 segment_id_ball = "ball"
+segment_id_robot = "robot"
 mujoco_id = "mj"
 model = "pamy" # i.e pamy.xml in pam_mujoco/models/
 
@@ -15,18 +16,22 @@ model = "pamy" # i.e pamy.xml in pam_mujoco/models/
 def execute_mujoco(segment_id_contacts,
                    segment_id_contacts_reset,
                    segment_id_ball,
+                   segment_id_robot,
                    mujoco_id,
                    model):
     # init mujoco
     pam_mujoco.init_mujoco()
     # adding contact detection between ball and table
+    # note: best to add first (i.e. before mirror robot and
+    # mirror ball), as controllers are called in sequence,
+    # and contact info will change behavior of mirror ball controller
     pam_mujoco.add_contact_ball(segment_id_contacts,
                                 segment_id_contacts_reset,
                                 "ball_free_jnt",
                                 "ball1",
-                                "plate")
+                                "racket")
     # adding the mirror ball controller
-    # (mirroring will be interrupted upon contact with the table:
+    # (mirroring will be interrupted upon contact with the racket:
     # the contact controller (added right above) will write contact information
     # in the shared memory (segment: segment_id_contacts). The ball mirroring 
     # controller will stop mirroring the ball after the data shared in the memory
@@ -35,6 +40,10 @@ def execute_mujoco(segment_id_contacts,
                                                  mujoco_id,
                                                  "ball_free_jnt",
                                                  segment_id_contacts)
+    # adding robot mirroring controller
+    pam_mujoco.add_mirror_robot(segment_id_robot,
+                                mujoco_id,
+                                "joint_base_rotation")
     # starting the thread
     pam_mujoco.execute(mujoco_id,model)
     # looping until requested to stop
@@ -47,40 +56,59 @@ process  = multiprocessing.Process(target=execute_mujoco,
                                    args=(segment_id_contacts,
                                          segment_id_contacts_reset,
                                          segment_id_ball,
+                                         segment_id_robot,
                                          mujoco_id,model,))
 process.start()
-time.sleep(1)
+time.sleep(3)
 
 # initializing o80 frontend for sending ball position/velocity
 # to mujoco thread
-frontend = pam_mujoco.MirrorOneBallFrontEnd(segment_id_ball)
+frontend_ball = pam_mujoco.MirrorOneBallFrontEnd(segment_id_ball)
 
-# reading a random pre-recorded ball trajectory
-trajectory_points = list(context.BallTrajectories().random_trajectory())
+# initializing o80 frontend for sending robot joints position/velocity
+# to mujoco thread
+frontend_robot = pam_mujoco.MirrorRobotFrontEnd(segment_id_robot)
 
 # having the ball falling off slowly
-start_point = [1,1,1]
-end_point = [1,1,-3]
+ball_x = 0.8
+ball_y = 0.1
+ball_z_start = 1.0
+ball_z_end = -3.0
+start_point = [ball_x,ball_y,ball_z_start]
+end_point = [ball_x,ball_y,ball_z_end]
 duration = 5.0 # seconds
 velocity = [0,0,(end_point[2]-start_point[2])/duration]
 # "teleportation" to start point
 for dim in range(3):
-    frontend.add_command(2*dim,
+    frontend_ball.add_command(2*dim,
                          o80.State1d(start_point[dim]),
                          o80.Mode.QUEUE)
 # falling down to end point
 for dim in range(3):
-    frontend.add_command(2*dim,
+    frontend_ball.add_command(2*dim,
                          o80.State1d(end_point[dim]),
                          o80.Duration_us.seconds(int(duration)),
                          o80.Mode.QUEUE)
-    frontend.add_command(2*dim+1,
+    frontend_ball.add_command(2*dim+1,
                          o80.State1d(velocity[dim]),
                          o80.Duration_us.seconds(int(duration)),
                          o80.Mode.QUEUE)
 
-# sending ball trajectory
-frontend.pulse();
+
+# having the robot going to a reference position
+robot_target = [np.pi/4.0,
+                0.0,
+                np.pi/2.0,
+                np.pi/4.0]
+robot_target = [o80.State2d(rt,0) for rt in robot_target]
+for dof,target in enumerate(robot_target):
+    frontend_robot.add_command(dof,target,
+                               o80.Duration_us.milliseconds(500),
+                               o80.Mode.QUEUE)
+    
+# sending ball trajectory and robot motion
+frontend_ball.pulse()
+frontend_robot.pulse()
 
 # bug ? during first iterations, contact are detected ...
 time.sleep(1)
