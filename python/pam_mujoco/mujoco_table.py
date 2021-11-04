09a6af905cb7668101d93b35b64c745ba2ebf0ec
Vincent Berenz <vberenz@tuebingen.mpg.de>
2021-11-04 17:35:40 +0100

    updated default values for table and robot

--- python/pam_mujoco/mujoco_table.py
+++ python/pam_mujoco/mujoco_table.py
@@ -2,7 +2,7 @@ class MujocoTable:
     def __init__(
         self,
         segment_id,
-        position=[0.1, 0.0, -0.44],
+        position=[0.8, 1.7, -0.475],
         orientation="-1 0 0 0 -1 0",
         size=[0.7625, 1.37, 0.02],
     ):
