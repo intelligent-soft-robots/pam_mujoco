35d9dc038e7b00df473b14879b6b2e5b23ec9e42
Vincent Berenz <vberenz@tuebingen.mpg.de>
2021-11-23 09:41:29 +0100

    applied formating on buster_controller.cpp

--- src/burster_controller.cpp
+++ src/burster_controller.cpp
@@ -2,7 +2,6 @@
 
 namespace pam_mujoco
 {
-  
 BursterController::BursterController(std::string mujoco_id)
     : burster_{mujoco_id}, first_iteration_{true}
 {
@@ -13,13 +12,13 @@ void BursterController::apply(const mjModel* m, mjData* d)
     (void)m;  // avoiding unused argument warning upon compilation
     if (this->must_update(d))
     {
-      if(!first_iteration_)
-	{
-	  burster_.pulse();
-	}
-      // we do not block the first iteration: allows pam_mujoco
-      // to start graphics properly.
-      first_iteration_ = false;
+        if (!first_iteration_)
+        {
+            burster_.pulse();
+        }
+        // we do not block the first iteration: allows pam_mujoco
+        // to start graphics properly.
+        first_iteration_ = false;
     }
 }
 
