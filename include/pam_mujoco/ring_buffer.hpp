791af3454e9418c5e7011c503985842a12b333c4
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-03-30 14:21:10 +0200

    RingBuffer: Remove get_all()
    It is not used anymore (use index access with operator[] instead to loop
    over the buffer).

--- include/pam_mujoco/ring_buffer.hpp
+++ include/pam_mujoco/ring_buffer.hpp
@@ -53,30 +53,6 @@ public:
         return buffer_[actual_i];
     }
 
-    /**
-     * @brief Get content of the buffer.
-     *
-     * The content of the buffer is reordered so that the oldest element is
-     * first.
-     *
-     * Important: This invalidates the reference of a previous call to
-     * current().
-     */
-    std::array<T, buffer_size>& get_all()
-    {
-        // rotate the buffer such that the elements are ordered by age (oldest
-        // first)
-        // start for the rotated buffer is index + 1 (= oldest element)
-        size_t start = (index_ + 1) % buffer_size;
-        std::rotate(buffer_.begin(), buffer_.begin() + start, buffer_.end());
-
-        // update index_ accordingly (active element is now at the end of the
-        // buffer)
-        index_ = buffer_size - 1;
-
-        return buffer_;
-    }
-
 private:
     std::array<T, buffer_size> buffer_;
     size_t index_ = 0;
--- tests/test_ring_buffer.cpp
+++ tests/test_ring_buffer.cpp
@@ -46,38 +46,3 @@ TEST(RingBuffer, test_index_access)
     rb[1] = 42;
     EXPECT_EQ(rb[1], 42);
 }
-
-TEST(RingBuffer, test_get_all)
-{
-    RingBuffer<int, 3> rb;
-
-    rb.current() = 1;
-    rb.next();
-    rb.current() = 2;
-    rb.next();
-    rb.current() = 3;
-
-    {
-        const auto &data = rb.get_all();
-        std::array<int, 3> expected = {1, 2, 3};
-        ASSERT_EQ(data, expected);
-    }
-
-    rb.next();
-    rb.current() = 4;
-
-    {
-        const auto &data = rb.get_all();
-        std::array<int, 3> expected = {2, 3, 4};
-        ASSERT_EQ(data, expected);
-    }
-
-    rb.next();
-    rb.current() = 5;
-
-    {
-        const auto &data = rb.get_all();
-        std::array<int, 3> expected = {3, 4, 5};
-        ASSERT_EQ(data, expected);
-    }
-}
