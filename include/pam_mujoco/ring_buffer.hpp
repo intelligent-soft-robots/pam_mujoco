38af032b9f8ef5caca70e247e78c3dd492f6e4e1
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-03-30 12:01:22 +0200

    RingBuffer: Add size() and index access

--- include/pam_mujoco/ring_buffer.hpp
+++ include/pam_mujoco/ring_buffer.hpp
@@ -9,12 +9,18 @@ namespace pam_mujoco
  * @brief Simple ring buffer using static memory.
  *
  * @tparam T Type of the buffer elements.
- * @tparam size Size of the buffer
+ * @tparam buffer_size Size of the buffer
  */
-template <typename T, size_t size>
+template <typename T, size_t buffer_size>
 class RingBuffer
 {
 public:
+    //! @brief Get size of the buffer.
+    size_t size() const
+    {
+        return buffer_size;
+    }
+
     /**
      * @brief Move to the next element.
      *
@@ -23,13 +29,28 @@ public:
      */
     void next()
     {
-        index_ = (index_ + 1) % size;
+        index_ = (index_ + 1) % buffer_size;
     }
 
     //! @brief Get writeable reference to the currently active element.
     T& current()
     {
         return buffer_[index_];
+    }
+
+    /**
+     * @brief Get writeable reference to i-th element of the buffer.
+     *
+     * The index 0 always refers to the oldest element in the buffer, 1 to the
+     * second oldest, and so on.
+     *
+     * No range check is performed, if ``i >= size()`` the behaviour is
+     * undefined.
+     */
+    T& operator[](std::size_t i)
+    {
+        std::size_t actual_i = (index_ + 1 + i) % buffer_size;
+        return buffer_[actual_i];
     }
 
     /**
@@ -41,23 +62,23 @@ public:
      * Important: This invalidates the reference of a previous call to
      * current().
      */
-    const std::array<T, size>& get_all()
+    std::array<T, buffer_size>& get_all()
     {
         // rotate the buffer such that the elements are ordered by age (oldest
         // first)
         // start for the rotated buffer is index + 1 (= oldest element)
-        size_t start = (index_ + 1) % size;
+        size_t start = (index_ + 1) % buffer_size;
         std::rotate(buffer_.begin(), buffer_.begin() + start, buffer_.end());
 
         // update index_ accordingly (active element is now at the end of the
         // buffer)
-        index_ = size - 1;
+        index_ = buffer_size - 1;
 
         return buffer_;
     }
 
 private:
-    std::array<T, size> buffer_;
+    std::array<T, buffer_size> buffer_;
     size_t index_ = 0;
 };
 }  // namespace pam_mujoco
--- tests/test_ring_buffer.cpp
+++ tests/test_ring_buffer.cpp
@@ -24,6 +24,29 @@ TEST(RingBuffer, test_basics)
     ASSERT_EQ(rb.current(), 4);
 }
 
+TEST(RingBuffer, test_index_access)
+{
+    RingBuffer<int, 3> rb;
+
+    // fill with data
+    rb.current() = 1;
+    rb.next();
+    rb.current() = 2;
+    rb.next();
+    rb.current() = 3;
+    rb.next();
+    rb.current() = 4;
+
+    // test read access
+    EXPECT_EQ(rb[0], 2);
+    EXPECT_EQ(rb[1], 3);
+    EXPECT_EQ(rb[2], 4);
+
+    // test write access
+    rb[1] = 42;
+    EXPECT_EQ(rb[1], 42);
+}
+
 TEST(RingBuffer, test_get_all)
 {
     RingBuffer<int, 3> rb;
